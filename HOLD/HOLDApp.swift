87f18abce5c1e436e223996ebe595c79de77443b
StamatisFloratos <156610393+StamatisFloratos@users.noreply.github.com>
2025-03-20 23:03:14 +0000

    Initial Commit

---
+++ HOLD/HOLDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOLDApp.swift
+//  HOLD
+//
+//  Created by Stamatis Floratos on 20/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HOLDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
